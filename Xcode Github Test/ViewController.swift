b005eaf6ae84510f45c954761b47a447837973bc
Amy Feng <afeng5@fordham.edu>
2019-06-24 19:15:25 -0400

    Initial Commit

---
+++ Xcode Github Test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Xcode Github Test
+//
+//  Created by Amy Feng on 6/24/19.
+//  Copyright © 2019 Amy Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let test = "test"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            print(test)
+    }
+
+
+}
+
---
+++ Xcode Github TestTests/Xcode_Github_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Xcode_Github_TestTests.swift
+//  Xcode Github TestTests
+//
+//  Created by Amy Feng on 6/24/19.
+//  Copyright © 2019 Amy Feng. All rights reserved.
+//
+
+import XCTest
+@testable import Xcode_Github_Test
+
+class Xcode_Github_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
