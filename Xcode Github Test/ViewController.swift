4aab2de22854a1769f8dd1ea4a587e9c369dae99
Amy Feng <afeng5@fordham.edu>
2019-06-24 19:16:59 -0400

    Change # 1 - removing message

--- Xcode Github Test/ViewController.swift
+++ Xcode Github Test/ViewController.swift
@@ -10,10 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let test = "test"
     override func viewDidLoad() {
         super.viewDidLoad()
-            print(test)
     }
 
 
